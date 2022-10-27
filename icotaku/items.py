e64cbb415bdd07daf2d3434fe0e815a9d5951f12
liberdi-ipssi <l.iberdi@ecole-ipssi.net>
2022-10-27 10:32:33 +0200

    items

--- icotaku/items.py
+++ icotaku/items.py
@@ -10,3 +10,28 @@ class IcotakuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+#À ajouter au fichier items.py
+class PlanningItem(scrapy.Item):
+    title = scrapy.Field()
+    desc = scrapy.Field()
+    origin = scrapy.Field()
+    distribution = scrapy.Field()
+    editor = scrapy.Field()
+    release = scrapy.Field()
+    category = scrapy.Field()
+    season = scrapy.Field()
+
+#À ajouter au fichier items.py
+class PlanningDetailItem(scrapy.Item):
+    titleFr = scrapy.Field()
+    titleJp = scrapy.Field()
+    origin = scrapy.Field()
+    genre = scrapy.Field()
+    public = scrapy.Field()
+    nbEpisode = scrapy.Field()
+    durationEpisode = scrapy.Field()
+    editor = scrapy.Field()
+    officialWebsite = scrapy.Field()
+    story = scrapy.Field()
+    img = scrapy.Field()
